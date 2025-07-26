5a4b8747a0c94fbc2a188a126a603caa7136ad6c
Gopal-nd <gopalhelpyou@gmail.com>
2025-07-26 15:59:43 +0530

    problem management

--- backend/src/controllers/problems.controller.ts
+++ backend/src/controllers/problems.controller.ts
@@ -21,7 +21,8 @@ export const createProblem = asyncHandler(async (req, res) => {
     referenceSolutions,
   } = req.body;
 
-  console.log("body", req.body);
+  // console.log("body", req.body);
+
   try {
     for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
       const languageId = getJudge0LanguageId(language);
@@ -70,26 +71,29 @@ export const createProblem = asyncHandler(async (req, res) => {
         }
       }
     }
-    //   const newProblem = await prisma.problems.create({
-    //   data: {
-    //     title,
-    //     description,
-    //     difficulty,
-    //     tags,
-    //     examples,
-    //     constraints,
-    //     testCases,
-    //     codeSnippets,
-    //     referenceSolutions,
-    //     userId: req.user?.id,
-    //   },
-    // });
 
-    return res.status(201).json({
-      sucess: true,
-      message: "Message Created Successfully",
-      problem: "newProblem",
+    if(!req.user) throw new APIError({status: 400, message: "User not found"})
+
+      const newProblem = await prisma.problems.create({
+      data: {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testCases,
+        codeSnippets,
+        referenceSolutions,
+        userId: req.user?.id,
+      },
     });
+
+    return new ApiResponse({
+      statusCode: 200,
+      data: newProblem,
+      message: "Success",
+    })
   } catch (error) {
     console.log(error);
     return new ApiResponse({
@@ -100,11 +104,190 @@ export const createProblem = asyncHandler(async (req, res) => {
   }
 });
 
-export const getAllProblems = asyncHandler(async (req, res) => {});
+export const getAllProblems = asyncHandler(async (req, res) => {
+  const problems = await prisma.problems.findMany();
 
-export const getProblemById = asyncHandler(async (req, res) => {});
+  if (!problems) {
+    return new ApiResponse({
+      statusCode: 400,
+      data: null,
+      message: "No problems found",
+    });
+  }
 
-export const updateProblemById = asyncHandler(async (req, res) => {});
-export const deleteProblemById = asyncHandler(async (req, res) => {});
+  return new ApiResponse({
+    statusCode: 200,
+    data: problems,
+    message: "Success",
+  })
+});
+
+export const getProblemById = asyncHandler(async (req, res) => {
+
+  const {id} = req.params
+
+
+  const problem = await prisma.problems.findUnique({
+    where: {
+      id: id ,
+    },
+  });
+
+  if (!problem) {
+    return new ApiResponse({
+      statusCode: 400,
+      data: null,
+      message: "No problems found",
+    });
+  }
+
+  return new ApiResponse({
+    statusCode: 200,
+    data: problem,
+    message: "Success",
+  })
+  })
+
+export const updateProblemById = asyncHandler(async (req, res) => {
+
+  const {id} = req.params
+     const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testCases,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body;
+  const problem = await prisma.problems.findUnique({
+    where: {
+      id: id ,
+    },
+  });
+
+  if (!problem) {
+    return new ApiResponse({
+      statusCode: 400,
+      data: null,
+      message: "No problems found",
+    });
+  }
+
+   try {
+    for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+      const languageId = getJudge0LanguageId(language);
+
+      if (!languageId) {
+        throw new APIError({
+          status: 400,
+          message: "Invalid languageId " + languageId,
+        });
+      }
+
+      const submissions = testCases.map(
+        ({ input, output }: { input: string; output: string }) => ({
+          source_code: solutionCode,
+          language_id: languageId,
+          stdin: input,
+          expected_output: output,
+        })
+      );
+
+      console.log( "submitions",submissions)
+
+      const submissionResults = await submitBatch(submissions);
+
+      const tokens = submissionResults.map((res: any) => res.token);
+    //   console.log("tokens are :-", tokens)
+      const results = await pollBatchResults(tokens);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        console.log( "result of each test case",i,result)
+        console.log("Result-----", result);
+        console.log(
+          `Testcase ${i + 1} and Language ${language} ----- result ${JSON.stringify(result.status.description)}`
+        );
+        if (result.status.id !== 3) {
+          return new ApiResponse({
+            statusCode: 400,
+            data: null,
+            message: `Testcase ${
+              i + 1
+            } and Language ${language} ----- result ${JSON.stringify(
+              result.status.description
+            )}`,
+          });
+        }
+      }
+    }
+
+    if(!req.user) throw new APIError({status: 400, message: "User not found"})
+
+      const updatedProblem = await prisma.problems.update({
+      data: {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testCases,
+        codeSnippets,
+        referenceSolutions,
+        userId: req.user?.id,
+      },
+      where:{
+        id
+      }
+    });
+
+    return new ApiResponse({
+      statusCode: 200,
+      data: updatedProblem,
+      message: "Success",
+    })
+  } catch (error) {
+    console.log(error);
+    return new ApiResponse({
+      statusCode: 400,
+      data: null,
+      message: `something went wrong ${error}`,
+    });
+  }
+
+});
+export const deleteProblemById = asyncHandler(async (req, res) => {
+  const {id} = req.params;
+
+  const problem = await prisma.problems.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!problem) {
+    return new ApiResponse({
+      statusCode: 400,
+      data: null,
+      message: "No problems found",
+    });
+  }
+
+  await prisma.problems.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  return new ApiResponse({
+    statusCode: 200,
+    data: null,
+    message: "Success",
+  })
+  })
 
 export const getUserSolvedProblems = asyncHandler(async (req, res) => {});
